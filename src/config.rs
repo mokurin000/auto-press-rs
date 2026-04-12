cafde97d1ba93cc2073705c1f70dfff4e5770854
莯凛 <mokurin000@gmail.com>
2026-04-13 06:48:40 +0800

    feat: support 0b, 0o, 0x prefixes

--- src/config.rs
+++ src/config.rs
@@ -7,7 +7,7 @@ use std::ops::RangeInclusive;
 #[argh(help_triggers("-h", "--help"))]
 pub struct Config {
     /// scan 1 make code
-    #[argh(option)]
+    #[argh(option, from_str_fn(parse_int))]
     pub scan_code: u16,
 
     /// press delay range start (milliseconds)
@@ -35,4 +35,17 @@ impl Config {
     pub fn hold_delay(&self) -> RangeInclusive<u32> {
         self.hold_delay_min..=self.hold_delay_max
     }
+}
+
+fn parse_int(s: &str) -> Result<u16, String> {
+    if let Some(x) = s.strip_prefix("0x") {
+        u16::from_str_radix(x, 16)
+    } else if let Some(x) = s.strip_prefix("0b") {
+        u16::from_str_radix(x, 2)
+    } else if let Some(x) = s.strip_prefix("0o") {
+        u16::from_str_radix(x, 8)
+    } else {
+        s.parse()
+    }
+    .map_err(|e| e.to_string())
 }
