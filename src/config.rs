98f3c6b86703b486e1960a4a0c3d8a2b9dd9fbd9
莯凛 <mokurin000@gmail.com>
2026-04-13 06:40:57 +0800

    fix: argh help requires the help feature

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,10 @@
 use argh::FromArgs;
 use std::ops::RangeInclusive;
 
-#[derive(FromArgs, Debug)]
 /// keyboard delay config
+#[derive(FromArgs, Debug)]
+#[argh(description = "Demo program to simulate keyboard input")]
+#[argh(help_triggers("-h", "--help"))]
 pub struct Config {
     /// scan 1 make code
     #[argh(option)]
@@ -10,27 +12,27 @@ pub struct Config {
 
     /// press delay range start (milliseconds)
     #[argh(option)]
-    pub press_delay_from: u32,
+    pub press_delay_min: u32,
 
-    /// press delay range end (milliseconds, inclusive)
+    /// press delay range end (milliseconds)
     #[argh(option)]
-    pub press_delay_to: u32,
+    pub press_delay_max: u32,
 
     /// hold delay range start (milliseconds)
     #[argh(option, default = "50")]
-    pub hold_delay_from: u32,
+    pub hold_delay_min: u32,
 
-    /// hold delay range end (milliseconds, inclusive)
+    /// hold delay range end (milliseconds)
     #[argh(option, default = "120")]
-    pub hold_delay_to: u32,
+    pub hold_delay_max: u32,
 }
 
 impl Config {
     pub fn press_delay(&self) -> RangeInclusive<u32> {
-        self.press_delay_from..=self.press_delay_to
+        self.press_delay_min..=self.press_delay_max
     }
 
     pub fn hold_delay(&self) -> RangeInclusive<u32> {
-        self.hold_delay_from..=self.hold_delay_to
+        self.hold_delay_min..=self.hold_delay_max
     }
 }
