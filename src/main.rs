d7091ef15ba62926b2f7ec27db8f761916cfd396
莯凛 <mokurin000@gmail.com>
2026-04-25 04:43:54 +0800

    refactor: extract scan code press logic

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 use std::{error::Error, time::SystemTime};
 
-use auto_press_rs::rng::NormalInRange;
-use interception::{Interception, KeyState, ScanCode, Stroke};
+use interception::Interception;
 use spdlog::{error, info};
 
-use auto_press_rs::utils::{find_keyboard, is_extended, sleep};
-
-pub mod config;
+use auto_press_rs::utils::{find_keyboard, press_key, sleep};
+use auto_press_rs::{config::Config, rng::NormalInRange};
 
 fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    let config @ config::Config { scan_code, .. } = argh::from_env();
+    let config @ Config { scan_code, .. } = argh::from_env();
     let start = SystemTime::now();
 
     let Some(interception) = Interception::new() else {
@@ -26,25 +24,6 @@ fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
 
     info!("Keyboard device: {keyboard}");
 
-    // FIXME: Pause, Break are unsupported
-    let extended_flag = if is_extended(scan_code) {
-        KeyState::E0
-    } else {
-        KeyState::empty()
-    };
-
-    let code = ScanCode::try_from(scan_code)?;
-    let stroke_down = Stroke::Keyboard {
-        code,
-        state: KeyState::DOWN | extended_flag,
-        information: 0,
-    };
-    let stroke_up = Stroke::Keyboard {
-        code,
-        state: KeyState::UP | extended_flag,
-        information: 0,
-    };
-
     loop {
         if config.run_duration != 0
             && let Ok(elapsed) = start.elapsed()
@@ -58,11 +37,6 @@ fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
         info!("Waiting for {wait}ms...");
         sleep(wait);
 
-        let press = rng.norm_rand(config.hold_duration());
-        info!("Pressing for {press}ms...");
-
-        interception.send(keyboard, &[stroke_down]);
-        sleep(press);
-        interception.send(keyboard, &[stroke_up]);
+        press_key(&config, &mut rng, &interception, keyboard, scan_code)?;
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,9 @@
-use std::{thread, time::Duration};
+use std::{error::Error, thread, time::Duration};
+
+use interception::{Device, Interception, KeyState, ScanCode, Stroke};
+use spdlog::info;
+
+use crate::{config::Config, rng::NormalInRange as _};
 
 pub fn sleep(ms: u32) {
     thread::sleep(Duration::from_millis(ms as _));
@@ -10,4 +15,41 @@ pub fn find_keyboard() -> Option<i32> {
 
 pub fn is_extended(scan_code: u16) -> bool {
     scan_code >> 8 == 0xE0
+}
+
+pub fn press_key(
+    config: &Config,
+    rng: &mut fastrand::Rng,
+    interception: &Interception,
+    keyboard: Device,
+    scan_code: u16,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    // FIXME: Pause, Break are unsupported
+    let extended_flag = if is_extended(scan_code) {
+        KeyState::E0
+    } else {
+        KeyState::empty()
+    };
+
+    let code = ScanCode::try_from(scan_code)?;
+
+    let stroke_down = Stroke::Keyboard {
+        code,
+        state: KeyState::DOWN | extended_flag,
+        information: 0,
+    };
+    let stroke_up = Stroke::Keyboard {
+        code,
+        state: KeyState::UP | extended_flag,
+        information: 0,
+    };
+
+    let press = rng.norm_rand(config.hold_duration());
+    info!("Pressing for {press}ms...");
+
+    interception.send(keyboard, &[stroke_down]);
+    sleep(press);
+    interception.send(keyboard, &[stroke_up]);
+
+    Ok(())
 }
