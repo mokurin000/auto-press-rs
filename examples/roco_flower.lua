8d8aa30b98af7a7b1ca5eb7c9fd48bcac8eca8e8
莯凛 <mokurin000@gmail.com>
2026-04-27 04:07:24 +0800

    chore: fix folder typo

--- examples/roco_flower.lua
+++ examples/roco_flower.lua
@@ -1,3 +1,6 @@
+local action_times = 100 -- 15 * 60 / 9 = 100
+local action_key = 0x4   -- 开心 3
+
 local action_min = 8000
 local action_max = 9000
 
@@ -10,19 +13,14 @@ local pet_place_max = 3000
 input_driver:delay(action_min, action_max)
 
 local adjust_time = {
-    0x01, -- Esc
-    0x21, -- F
+    0x01, -- Esc, 首次启动时应该在星盘或动作界面
+    0x21, -- F, 交互移动魔力之源
     0x02, -- 1 消磨时间
     0x02, -- 1 到早上
     0x02, -- 2 休息并离开
 }
 
 while true do
-    for _ = 1, 100 do
-        input_driver:key_press(0x04) -- '3', 开心
-        input_driver:delay(action_min, action_max)
-    end
-
     for _, scan_code in adjust_time
     do
         input_driver:key_press(scan_code)
@@ -35,6 +33,11 @@ while true do
         input_driver:delay(pet_place_min, pet_place_max)
     end
 
-    input_driver:key_press(0x000F)
+    input_driver:key_press(0x000F) -- Tab
     input_driver:delay(pet_place_min, pet_place_max)
+
+    for _ = 1, action_times do
+        input_driver:key_press(action_key)
+        input_driver:delay(action_min, action_max)
+    end
 end
